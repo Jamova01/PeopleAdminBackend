de1b1b64be7a25207e2dc32563ef4d77bdbed13a
Jamova01 <jorgemova01@gmail.com>
2024-07-07 14:21:01 -0500

    Enhance TaskService to handle errors with boom for improved HTTP error responses

--- services/task.service.js
+++ services/task.service.js
@@ -6,35 +6,55 @@ class TaskService {
   constructor() {}
 
   async create(data) {
-    const newTask = await models.Task.create(data);
-    return newTask;
+    try {
+      const newTask = await models.Task.create(data);
+      return newTask;
+    } catch (error) {
+      throw boom.badImplementation('Error creating task', error);
+    }
   }
 
   async find() {
-    const tasks = await models.Task.findAll({
-      include: ['person']
-    });
-    return tasks;
+    try {
+      const tasks = await models.Task.findAll({
+        include: ['person'],
+      });
+      return tasks;
+    } catch (error) {
+      throw boom.badImplementation('Error finding tasks', error);
+    }
   }
 
   async findOne(id) {
-    const task = await models.Task.findByPk(id);
-    if (!task) {
-      throw boom.notFound('task not found');
+    try {
+      const task = await models.Task.findByPk(id);
+      if (!task) {
+        throw boom.notFound('task not found');
+      }
+      return task;
+    } catch (error) {
+      throw boom.badImplementation('Error finding task', error);
     }
-    return task;
   }
 
   async update(id, changes) {
-    const task = await this.findOne(id);
-    const response = await task.update(changes);
-    return response;
+    try {
+      const task = await this.findOne(id);
+      const response = await task.update(changes);
+      return response;
+    } catch (error) {
+      throw boom.badImplementation('Error updating task', error);
+    }
   }
 
   async delete(id) {
-    const task = await this.findOne(id);
-    await task.destroy();
-    return { id };
+    try {
+      const task = await this.findOne(id);
+      await task.destroy();
+      return { id };
+    } catch (error) {
+      throw boom.badImplementation('Error deleting task', error);
+    }
   }
 }
 
