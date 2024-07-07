bc63fc09166b4500c84ef4331cbf9d13cb98abc1
Jamova01 <jorgemova01@gmail.com>
2024-07-06 22:20:19 -0500

    Add Task model definition

---
+++ db/models/task.model.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { PERSON_TABLE } = require('./person.model');
+
+const TASK_TABLE = 'tasks';
+
+const TaskSchema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+
+  title: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+
+  description: {
+    allowNull: false,
+    type: DataTypes.TEXT,
+  },
+
+  deadline: {
+    allowNull: false,
+    type: DataTypes.DATE,
+  },
+
+  created_at: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+
+  person_id: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: PERSON_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+};
+
+class Task extends Model {
+  static assocciate(models) {
+    this.belongsTo(models.Person, {
+      as: 'person',
+    });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: TASK_TABLE,
+      modelName: 'Task',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { TASK_TABLE, TaskSchema, Task };
